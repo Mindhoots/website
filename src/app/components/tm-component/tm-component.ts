a6376e33f6f4d1f37f66110463df87b83b4c7517
chandru <ch4ndru.13@gmail.com>
2026-03-03 17:51:15 +0530

    faq completed

--- src/app/components/tm-component/tm-component.ts
+++ src/app/components/tm-component/tm-component.ts
@@ -1,24 +1,44 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { CarouselComponent } from '../../shared/carousel-component/carousel-component';
 import { PricingComponent } from '../pricing-component/pricing-component';
 import { FaqComponent } from '../faq-component/faq-component';
-
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tm-component',
-  imports: [MatButtonToggleModule, CarouselComponent, PricingComponent, FaqComponent],
+  imports: [MatButtonToggleModule, CarouselComponent, PricingComponent, FaqComponent, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './tm-component.html',
   styleUrl: './tm-component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class TmComponent {
-  planRange = signal<'monthly' | 'annually'>('monthly');
+  private formBuilder = inject(FormBuilder);
 
-  updatePlanRange(event: any) {
-    this.planRange.set(event);
+  isYearlyPricing: boolean = true;
+
+  enquiryForms = this.formBuilder.group({
+    firstName: [''],
+    secondName: [''],
+    email: [''],
+    companyName: [''],
+    mobileNo: [''],
+    companySize: [''],
+    jobTitle: [''],
+    description: [''],
+  })
+
+  onSubmitForm() {
+
   }
+
+
+  updatePricing() {
+    this.isYearlyPricing = !this.isYearlyPricing;
+  }
+
 
   goToNewTab() {
     window.open('http://mindhootlabs.cloud:8080/', '_blank');
@@ -28,8 +48,32 @@ export class TmComponent {
 
   }
 
+  getEconomyPlan() {
+    if (this.isYearlyPricing) {
+      return this.yearlyEconomy;
+    } else {
+      return this.monthlyEconomy;
+    }
+  }
+
+  getPremiumPlan() {
+    if (this.isYearlyPricing) {
+      return this.yearlyPremium;
+    } else {
+      return this.monthlyPremium;
+    }
+  }
+
+  getPlusPlan() {
+    if (this.isYearlyPricing) {
+      return this.yearlyPlus;
+    } else {
+      return this.monthlyPlus;
+    }
+  }
+
   /// amount in dollars
-  economyPlan = {
+  monthlyEconomy = {
     amount: "400",
     name: 'Economy',
     details: [
@@ -41,7 +85,20 @@ export class TmComponent {
       "No customization",
     ]
   };
-  premiumPlan = {
+  yearlyEconomy = {
+    amount: "4500",
+    name: 'Economy',
+    details: [
+      "10users per org",
+      "5projects per org",
+      "100 tasks per org",
+      "10gb storage",
+      "2months history",
+      "No customization",
+    ]
+  };
+
+  monthlyPremium = {
     amount: "600",
     name: 'Premium',
     details: [
@@ -53,7 +110,21 @@ export class TmComponent {
       "No customization",
     ]
   };
-  plusPlan = {
+
+  yearlyPremium = {
+    amount: "12800",
+    name: 'Premium',
+    details: [
+      "20users per org",
+      "10projects per org",
+      "300 tasks per org",
+      "20gb storage",
+      "6months history",
+      "No customization",
+    ]
+  };
+
+  monthlyPlus = {
     amount: "1000",
     name: 'Premium Plus',
     details: [
@@ -66,17 +137,18 @@ export class TmComponent {
     ]
   };
 
-  customerPlan = {
-    amount: "",
-    name: 'Custom',
+  yearlyPlus = {
+    amount: "11000",
+    name: 'Premium Plus',
     details: [
-      "Unlimited per org",
-      "Unlimited per org",
-      "Unlimited per org",
-      "Unlimited storage",
-      "Unlimited history",
-      "Full Customization",
+      "unlimited users",
+      "unlimited projecs",
+      "unlimted tasks",
+      "unlimited storage",
+      "One year history",
+      "Limited customization",
     ]
   };
+
 
 }
